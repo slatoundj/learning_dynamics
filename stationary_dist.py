9b1cd9fbc3ebaac86a0e2d0583c9304d0b302194
Luka <Luka.Bonheure@ulb.be>
2024-01-05 10:58:48 +0100

    group achievement

--- stationary_dist.py
+++ stationary_dist.py
@@ -2,6 +2,7 @@ from math import (floor, exp, comb)
 import numpy as np
 from matplotlib import pyplot as plt
 import time
+import scipy as sc
 
 start = time.time()
 
@@ -198,114 +199,127 @@ P t - lambda I t = 0
 (P - lambda I) t = 0
 """
 
+def null(a, rtol=1e-8):
+    u, s, v = np.linalg.svd(a)
+    rank = (s > rtol*s[0]).sum()
+    return rank, v[rank:].T.copy()
 
-w_matrix = np.zeros((numStates, numStates))
-# q:row ; p:column
-for q in range(numStates):
-    q_config = rec_V(q)
-    q_ir, q_ip = q_config
-    for p in range(numStates):
-        if q == 0 and p == 0:
-            pr1 = proba(q_config, {"strategy": "Defect", "w_class": "Rich"}, mu, beta, h, r)        # Probability that state (ir, ip) switch to state (ir+1, ip)
-            pr2 = proba(q_config, {"strategy": "Defect", "w_class": "Poor"}, mu, beta, h, r)        # Probability that state (ir, ip) switch to state (ir, ip+1)
-            w_matrix[q, p] = 1 - pr1 - pr2    
-        elif p == q + 1:
-            w_matrix[q, p] = proba(q_config, {"strategy": "Defect", "w_class": "Rich"}, mu, beta, h, r)     # Probability that state (ir, ip) switch to state (ir+1, ip)
-        elif p == q + Zr + 1:
-            w_matrix[q, p] = proba(q_config, {"strategy": "Defect", "w_class": "Poor"}, mu, beta, h, r)     # Probability that state (ir, ip) switch to state (ir, ip+1)
-        elif p == q - 1:
-            w_matrix[q, p] = proba(q_config, {"strategy": "Cooperate", "w_class": "Rich"}, mu, beta, h, r)  # Probability that state (ir, ip) switch to state (ir-1, ip)
-        elif p == q - Zr - 1:
-            w_matrix[q, p] = proba(q_config, {"strategy": "Cooperate", "w_class": "Poor"}, mu, beta, h, r)  # Probability that state (ir, ip) switch to state (ir, ip-1)
-            
-        elif p == q:
-            pr1 = proba(q_config, {"strategy": "Defect", "w_class": "Rich"}, mu, beta, h, r)        # Probability that state (ir, ip) switch to state (ir+1, ip)
-            pr2 = proba(q_config, {"strategy": "Defect", "w_class": "Poor"}, mu, beta, h, r)        # Probability that state (ir, ip) switch to state (ir, ip+1)
-            pr3 = proba(q_config, {"strategy": "Cooperate", "w_class": "Rich"}, mu, beta, h, r)     # Probability that state (ir, ip) switch to state (ir-1, ip)
-            pr4 = proba(q_config, {"strategy": "Cooperate", "w_class": "Poor"}, mu, beta, h, r)     # Probability that state (ir, ip) switch to state (ir, ip-1)
-            w_matrix[q, p] = 1 - pr1 - pr2 - pr3 - pr4
-    print("\r", q, end=" ", flush=True)
-print("")
 
-for q in range(numStates):
-    s = w_matrix[q].sum()
-    if s < 0.999999 and s > 1.00001:
-        print(q, s)
+def transition_matrix(mu, beta, h, r):
+    w_matrix = np.zeros((numStates, numStates))
+    # q:row ; p:column
+    for q in range(numStates):
+        q_config = rec_V(q)
+        q_ir, q_ip = q_config
+        for p in range(numStates):
+            if q == 0 and p == 0:
+                pr1 = proba(q_config, {"strategy": "Defect", "w_class": "Rich"}, mu, beta, h, r)        # Probability that state (ir, ip) switch to state (ir+1, ip)
+                pr2 = proba(q_config, {"strategy": "Defect", "w_class": "Poor"}, mu, beta, h, r)        # Probability that state (ir, ip) switch to state (ir, ip+1)
+                w_matrix[q, p] = 1 - pr1 - pr2    
+            elif p == q + 1:
+                w_matrix[q, p] = proba(q_config, {"strategy": "Defect", "w_class": "Rich"}, mu, beta, h, r)     # Probability that state (ir, ip) switch to state (ir+1, ip)
+            elif p == q + Zr + 1:
+                w_matrix[q, p] = proba(q_config, {"strategy": "Defect", "w_class": "Poor"}, mu, beta, h, r)     # Probability that state (ir, ip) switch to state (ir, ip+1)
+            elif p == q - 1:
+                w_matrix[q, p] = proba(q_config, {"strategy": "Cooperate", "w_class": "Rich"}, mu, beta, h, r)  # Probability that state (ir, ip) switch to state (ir-1, ip)
+            elif p == q - Zr - 1:
+                w_matrix[q, p] = proba(q_config, {"strategy": "Cooperate", "w_class": "Poor"}, mu, beta, h, r)  # Probability that state (ir, ip) switch to state (ir, ip-1)
+                
+            elif p == q:
+                pr1 = proba(q_config, {"strategy": "Defect", "w_class": "Rich"}, mu, beta, h, r)        # Probability that state (ir, ip) switch to state (ir+1, ip)
+                pr2 = proba(q_config, {"strategy": "Defect", "w_class": "Poor"}, mu, beta, h, r)        # Probability that state (ir, ip) switch to state (ir, ip+1)
+                pr3 = proba(q_config, {"strategy": "Cooperate", "w_class": "Rich"}, mu, beta, h, r)     # Probability that state (ir, ip) switch to state (ir-1, ip)
+                pr4 = proba(q_config, {"strategy": "Cooperate", "w_class": "Poor"}, mu, beta, h, r)     # Probability that state (ir, ip) switch to state (ir, ip-1)
+                w_matrix[q, p] = 1 - pr1 - pr2 - pr3 - pr4
+        #print("\r", q, end=" ", flush=True)
+    #print("")
+    return w_matrix
         
+"""
+P = transition_matrix(1/Z, 3, h=0.7, r=0.3)
 
-pt = w_matrix.transpose()
-
-eigenvalues, eigenvectors = np.linalg.eig(pt)
-
-index_eigenvalue_1 = np.where(np.isclose(eigenvalues, 1))[0][0]
-
-steady_state_distribution = np.real(eigenvectors[:, index_eigenvalue_1])
-
-print("sum stationnary distrib", steady_state_distribution.sum())
-
-steady_state_distribution /= np.sum(steady_state_distribution)
-
-
-matrix = np.zeros((Zr+1, Zp+1))
-for i in range(numStates):
-    ir, ip = rec_V(i)
-    matrix[ir, ip] = steady_state_distribution[i]
-    
-plt.matshow(matrix)
+Q = (np.eye(P.shape[0]) - P).transpose()
+rank, null_space = null(Q)
+print(null_space.shape)
+pi = np.abs(null_space[:,0])
+pi = pi/pi.sum()
+print(pi.sum())
 
 plt.figure("stationary distrib")
-x = np.arange(numStates)
-plt.plot(x, steady_state_distribution)
+x = np.arange(pi.shape[0])
+plt.plot(x, pi)
 plt.show()
 
-"""
-Q = pt - np.eye(pt.shape[0])
-
-Q[-1, :] = 1
-
-steady_state_distribution = np.linalg.lstsq(Q, np.zeros(pt.shape[0]), rcond=None)[0]
-
-print("sum stationnary distrib", steady_state_distribution.sum())
-
-steady_state_distribution /= np.sum(steady_state_distribution)
-
-matrix = np.zeros((Zr+1, Zp+1))
+matrix = np.zeros((Zp+1, Zr+1))
 for i in range(numStates):
     ir, ip = rec_V(i)
-    matrix[ir, ip] = steady_state_distribution[i]
-    
-plt.matshow(matrix)
-"""
-
-"""
-
-valp, vecp = np.linalg.eig(w_matrix)
-print("valp:", valp)
-print("vecp:", vecp)
-index_eigenvalue_1 = np.where(np.isclose(valp, 1))[0][0]
-print(index_eigenvalue_1)
-eigenvector_1 = vecp[:,index_eigenvalue_1]
-print(eigenvector_1)
-print(eigenvector_1.sum())
-
-
-matrix = np.zeros((Zr+1, Zp+1))
-for i in range(numStates):
-    ir, ip = rec_V(i)
-    matrix[ir, ip] = np.abs(eigenvector_1[i])
+    matrix[ip, ir] = pi[i]
     
 plt.matshow(matrix)
 
+m = matrix
 
-for i, val in enumerate(valp):
-    #if np.real(val) == 1.0 and np.imag(val) == 0.0:
-    if np.abs(val) == 1.0:
-        result = vecp[i]
-        print("find a result !")
+#Define a colormap for the plot
+cmap = plt.cm.get_cmap('gray')
 
-        
+#Normalize the values in the matrix to map to colors
+norm = plt.Normalize(m.min(), m.max())
+
+#Create a scatter plot
+fig, ax = plt.subplots()
+for i in range(m.shape[0]):
+    for j in range(m.shape[1]):
+        color = (m[i][j] - m.min())/(m.max()-m.min())
+        ax.scatter(j, i, c=[[1-color,1-color,1-color]], alpha=1, s=2)  # j and i are reversed to match matrix indexing
+
+#Set the aspect ratio to make sure dots are square
+ax.set_aspect('equal')
+
+#Show the plot
+plt.show()
+"""
+
+def compute_stationary_distribution(mu, beta, h, r):
+    P = transition_matrix(mu, beta, h, r)
+    Q = (np.eye(P.shape[0]) - P).transpose()
+    rank, null_space = null(Q)
+    pi = np.abs(null_space[:,0])
+    pi = pi/pi.sum()
+    return pi
+
+
+def aG(i, r):
+    (ir, ip) = i
+    ag_i = ir * fitness((ir, ip), "Cooperate", "Rich", r)
+    ag_i += (Zr-ir) * fitness((ir, ip), "Defect", "Rich", r)
+    ag_i += ip * fitness((ir, ip), "Cooperate", "Poor", r)
+    ag_i += (Zp - ip) * fitness((ir, ip), "Defect", "Poor", r)
+    return ag_i/Z
+
+
+def eta_g(stationary_distribution, r):
+    eta_g_i = 0
+    for ir in range(Zr+1):
+        for ip in range(Zp+1):
+            i = V((ir, ip))
+            eta_g_i += stationary_distribution[i] * aG((ir, ip), r)
+    return eta_g_i/numStates
+
+
+def compute_group_achivement_in_function_of_r(mu, beta, h):
+    risk = [r/100 for r in range(101)]
+    eta_G = []
+    for r in risk:
+        pi = compute_stationary_distribution(mu, beta, h, r)
+        eta_G.append(eta_g(pi, r))
+    return risk, eta_G
+
+
+risk, eta_G = compute_group_achivement_in_function_of_r(mu=1/Z, beta=3, h=0.0)
+
+plt.figure("Group achievement in function of risk")
+plt.plot(risk, eta_G)
+
+
 end = time.time()
-
 print("elapsed time =", end-start, "seconds")
-
-"""
